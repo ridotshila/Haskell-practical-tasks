64cd50c550975a4032bed2212b06056e3b16367c
ridotshila <168440188+ridotshila@users.noreply.github.com>
2025-09-20 19:20:26 +0200

    Update chapter19.hs

--- chapter19.hs
+++ chapter19.hs
@@ -1,107 +1,191 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Main where
-import Data.Char (toLower)
+import Control.Applicative
+import Control.Monad (forever, when, replicateM)
+import Data.Foldable (sequenceA)
 
 ----------------------------------
--- HC18T1: mapToLower Function
+-- HC19T1: Applicative Instance for Pair
 ----------------------------------
-mapToLower :: String -> String
-mapToLower = fmap toLower
+data Pair a = Pair a a deriving (Show)
+
+instance Functor Pair where
+  fmap f (Pair x y) = Pair (f x) (f y)
+
+instance Applicative Pair where
+  pure x = Pair x x
+  (Pair f g) <*> (Pair x y) = Pair (f x) (g y)
 
 ----------------------------------
--- HC18T2: Functor Instance for Tree
+-- HC19T2: addThreeApplicative
 ----------------------------------
-data Tree a = Empty
-            | Node a (Tree a) (Tree a)
-            deriving (Show, Functor)
+addThreeApplicative :: Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int
+addThreeApplicative = liftA3 (\x y z -> x + y + z)
 
 ----------------------------------
--- HC18T3: incrementTreeValues
+-- HC19T3: safeProduct
 ----------------------------------
-incrementTreeValues :: Num a => Tree a -> Tree a
-incrementTreeValues = fmap (+1)
+safeProduct :: [Maybe Int] -> Maybe Int
+safeProduct = fmap product . sequenceA
 
 ----------------------------------
--- HC18T4: mapToBits
+-- HC19T4: liftAndMultiply
 ----------------------------------
-mapToBits :: [Bool] -> [Char]
-mapToBits = fmap (\b -> if b then '1' else '0')
+liftAndMultiply :: Maybe Int -> Maybe Int -> Maybe Int
+liftAndMultiply = liftA2 (*)
 
 ----------------------------------
--- HC18T5: Functor Instance for Either
+-- HC19T5: applyEffects with <*>
 ----------------------------------
--- Already in Prelude, but we re-implement:
-instance Functor (Either e) where
-  fmap _ (Left e)  = Left e
-  fmap f (Right x) = Right (f x)
+applyEffects :: (IO Int, IO Int) -> IO Int
+applyEffects (a, b) = (\x y -> x + y) <$> a <*> b
 
 ----------------------------------
--- HC18T6: applyToMaybe
+-- HC19T6: repeatEffect
 ----------------------------------
-applyToMaybe :: (a -> b) -> Maybe a -> Maybe b
-applyToMaybe = fmap
+repeatEffect :: IO () -> IO ()
+repeatEffect = forever
 
 ----------------------------------
--- HC18T7: fmapTuple
+-- HC19T7: conditionalPrint
 ----------------------------------
-fmapTuple :: (b -> c) -> (a, b) -> (a, c)
-fmapTuple = fmap
+conditionalPrint :: Bool -> String -> IO ()
+conditionalPrint cond msg = when cond (putStrLn msg)
 
 ----------------------------------
--- HC18T8: identityLawCheck
+-- HC19T8: discardSecond with <*
 ----------------------------------
-identityLawCheck :: (Eq (f a), Functor f) => f a -> Bool
-identityLawCheck x = fmap id x == x
+discardSecond :: Applicative f => f a -> f b -> f a
+discardSecond = (<*)
 
 ----------------------------------
--- HC18T9: compositionLawCheck
+-- HC19T9: pureAndApply
 ----------------------------------
-compositionLawCheck :: (Eq (f c), Functor f)
-                    => (b -> c) -> (a -> b) -> f a -> Bool
-compositionLawCheck f g x =
-  fmap (f . g) x == (fmap f . fmap g) x
+pureAndApply :: IO ()
+pureAndApply = do
+  let f = pure (+3) <*> Just 7
+  print f
 
 ----------------------------------
--- HC18T10: nestedFmap
+-- HC19T10: combineResults for Either
 ----------------------------------
-nestedFmap :: (a -> b) -> [[Maybe a]] -> [[Maybe b]]
-nestedFmap = fmap . fmap . fmap
+combineResults :: Either String Int -> Either String Int -> Either String Int
+combineResults = liftA2 (+)
+
+----------------------------------
+-- HC19T11: Applicative Instance for Wrapper
+----------------------------------
+newtype Wrapper a = Wrapper a deriving (Show)
+
+instance Functor Wrapper where
+  fmap f (Wrapper x) = Wrapper (f x)
+
+instance Applicative Wrapper where
+  pure = Wrapper
+  (Wrapper f) <*> (Wrapper x) = Wrapper (f x)
+
+----------------------------------
+-- HC19T12: sumThreeApplicative
+----------------------------------
+sumThreeApplicative :: Either String Int -> Either String Int -> Either String Int -> Either String Int
+sumThreeApplicative = liftA3 (\x y z -> x + y + z)
+
+----------------------------------
+-- HC19T13: whenApplicative
+----------------------------------
+whenApplicative :: Bool -> IO () -> IO ()
+whenApplicative cond action = when cond action
+
+----------------------------------
+-- HC19T14: replicateEffect
+----------------------------------
+replicateEffect :: Int -> IO a -> IO [a]
+replicateEffect = replicateM
+
+----------------------------------
+-- HC19T15: sequenceEffects
+----------------------------------
+sequenceEffects :: Applicative f => [f a] -> f [a]
+sequenceEffects = sequenceA
+
+----------------------------------
+-- HC19T16: applyWithEffects
+----------------------------------
+applyWithEffects :: Applicative f => f (a -> b) -> f a -> f b
+applyWithEffects = (<*>)
+
+----------------------------------
+-- HC19T17: simulateMaybeEffect
+----------------------------------
+simulateMaybeEffect :: Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int
+simulateMaybeEffect = liftA3 (\x y z -> x * y + z)
+
+----------------------------------
+-- HC19T18: combineEitherResults
+----------------------------------
+combineEitherResults :: Either String Int -> Either String Int -> Either String Int -> Either String Int
+combineEitherResults = liftA3 (\x y z -> x + y + z)
+
+----------------------------------
+-- HC19T19: sequenceApplicative
+----------------------------------
+sequenceApplicative :: [Maybe a] -> Maybe [a]
+sequenceApplicative = sequenceA
+
+----------------------------------
+-- HC19T20: replicateForever
+----------------------------------
+replicateForever :: IO () -> IO ()
+replicateForever = forever
 
 ----------------------------------
 -- Main for Demo
 ----------------------------------
 main :: IO ()
 main = do
-  putStrLn "\n--- HC18T1: mapToLower ---"
-  print (mapToLower "HELLO FunCtOrS")
+  putStrLn "\n--- HC19T1: Pair Applicative ---"
+  print $ (Pair (+1) (*2)) <*> (Pair 3 4)
 
-  putStrLn "\n--- HC18T2 & HC18T3: Tree Functor + Increment ---"
-  let tree = Node 5 (Node 3 Empty Empty) (Node 7 Empty Empty)
-  print tree
-  print (incrementTreeValues tree)
+  putStrLn "\n--- HC19T2: addThreeApplicative ---"
+  print $ addThreeApplicative (Just 1) (Just 2) (Just 3)
 
-  putStrLn "\n--- HC18T4: mapToBits ---"
-  print (mapToBits [True, False, True, True])
+  putStrLn "\n--- HC19T3: safeProduct ---"
+  print $ safeProduct [Just 2, Just 3, Just 4]
+  print $ safeProduct [Just 2, Nothing, Just 4]
 
-  putStrLn "\n--- HC18T5: Either Functor ---"
-  print (fmap (+1) (Right 10 :: Either String Int))
-  print (fmap (+1) (Left "error" :: Either String Int))
+  putStrLn "\n--- HC19T4: liftAndMultiply ---"
+  print $ liftAndMultiply (Just 3) (Just 5)
 
-  putStrLn "\n--- HC18T6: applyToMaybe ---"
-  print (applyToMaybe (*2) (Just 10))
-  print (applyToMaybe (*2) Nothing)
+  putStrLn "\n--- HC19T5: applyEffects ---"
+  sumResult <- applyEffects (print 3 >> return 3, print 4 >> return 4)
+  print sumResult
 
-  putStrLn "\n--- HC18T7: fmapTuple ---"
-  print (fmapTuple length ("tag", "hello"))
+  putStrLn "\n--- HC19T7: conditionalPrint ---"
+  conditionalPrint True "This is printed"
+  conditionalPrint False "This is not printed"
 
-  putStrLn "\n--- HC18T8: identityLawCheck ---"
-  print (identityLawCheck (Just 5))
-  print (identityLawCheck [1,2,3])
+  putStrLn "\n--- HC19T8: discardSecond ---"
+  print $ discardSecond (Just "First") (Just "Second")
 
-  putStrLn "\n--- HC18T9: compositionLawCheck ---"
-  print (compositionLawCheck (+1) (*2) (Just 3))
-  print (compositionLawCheck (++"!") reverse ["hi","yo"])
+  putStrLn "\n--- HC19T9: pureAndApply ---"
+  pureAndApply
 
-  putStrLn "\n--- HC18T10: nestedFmap ---"
-  print (nestedFmap (+1) [[Just 1, Nothing], [Just 2, Just 3]])
+  putStrLn "\n--- HC19T10: combineResults ---"
+  print $ combineResults (Right 5) (Right 7)
+
+  putStrLn "\n--- HC19T11: Wrapper Applicative ---"
+  print $ (Wrapper (*2)) <*> (Wrapper 10)
+
+  putStrLn "\n--- HC19T12: sumThreeApplicative ---"
+  print $ sumThreeApplicative (Right 1) (Right 2) (Right 3)
+
+  putStrLn "\n--- HC19T14: replicateEffect ---"
+  results <- replicateEffect 3 (return "Hi")
+  print results
+
+  putStrLn "\n--- HC19T15: sequenceEffects ---"
+  print $ sequenceEffects [Just 1, Just 2, Just 3]
+
+  putStrLn "\n--- HC19T16: applyWithEffects ---"
+  print $ applyWithEffects
